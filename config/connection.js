a229b2199ac624bcec952b1d68b53009b5018037
Derek Glassick <derek.glassick@gmail.com>
2020-05-09 14:29:08 -0700

    added the jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -6,17 +6,17 @@ var connection;
 if(process.env.JAWSDB_URL){
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{mysql.createConnection({
-    host: "localhost",
+    host: "l3855uft9zao23e2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
 
     // Your port; if not 3306
     port: 3306,
 
     // Your username
-    user: "root",
+    user: "n44plfg573zrvzs4",
 
     // Your password
-    password: "12345678",
-    database: "burgers_db",
+    password: "ub0sf4n7k8o1ch2x",
+    database: "xn6i7yjo689la5yv",
 
 });
 
